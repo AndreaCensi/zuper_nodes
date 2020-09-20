660d9f338575ba8a0db0a5b4015f32a81f42a489
Andrea Censi <acensi@ethz.ch>
2020-09-20 16:52:32 +0200

    better format

--- src/zuper_nodes/compatibility.py
+++ src/zuper_nodes/compatibility.py
@@ -1,5 +1,6 @@
 from zuper_commons.types import ZException
 from zuper_typing import can_be_used_as2
+
 from .language import InteractionProtocol
 
 __all__ = ["IncompatibleProtocol", "check_compatible_protocol"]
@@ -18,24 +19,28 @@ def check_compatible_protocol(p1: InteractionProtocol, p2: InteractionProtocol):
 
             if not k in p1.inputs:
                 msg = f'First protocol misses input "{k}".'
-                raise IncompatibleProtocol(msg)
+                raise IncompatibleProtocol(msg, p1_inputs=p1.inputs, p2_inputs=p2.inputs)
             v1 = p1.inputs[k]
             r = can_be_used_as2(v1, v2)
             if not r:
-                msg = f'For input "{k}", cannot use type {v1} as {v2}: {r}'
-                raise IncompatibleProtocol(msg)
+                msg = f'For input "{k}", cannot use type v1 as v2.'
+                raise IncompatibleProtocol(
+                    msg, k=k, v1=v1, v2=v2, r=r, p1_inputs=p1.inputs, p2_inputs=p2.inputs
+                )
 
         # check output compatibility
         # we should have all inputs
         for k, v2 in p2.outputs.items():
             if not k in p1.outputs:
                 msg = f'First protocol misses output "{k}".'
-                raise IncompatibleProtocol(msg)
+                raise IncompatibleProtocol(msg, p1_outputs=p1.outputs, p2_outputs=p2.outputs)
             v1 = p1.outputs[k]
             r = can_be_used_as2(v1, v2)
             if not r:
-                msg = f'For output "{k}", cannot use type {v1} as {v2}: {r}'
-                raise IncompatibleProtocol(msg)
+                msg = f'For output "{k}", cannot use type v1 as v2.'
+                raise IncompatibleProtocol(
+                    msg, k=k, v1=v1, v2=v2, r=r, p1_outputs=p1.outputs, p2_outputs=p2.outputs
+                )
             # XXX: to finish
     except IncompatibleProtocol as e:
         msg = "Cannot say that p1 is a sub-protocol of p2"
