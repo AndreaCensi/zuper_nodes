881b3270ee61f1f8451abd6217b70934df781d03
Andrea Censi <acensi@ethz.ch>
2021-04-24 20:03:42 +0200

    Bump to version 7.1.2104242003

--- src/zuper_nodes/__init__.py
+++ src/zuper_nodes/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "7.1.2104241833"
-__date__ = "2021-04-24T18:33:41.783335"
+__version__ = "7.1.2104242003"
+__date__ = "2021-04-24T20:03:42.569775"
 
 from zuper_commons.logs import ZLogger
 
